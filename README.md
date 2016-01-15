0259beb2d0f864c4bab0cc0f4a22d4ebab0568c4
Julien Gilli <julien.gilli@joyent.com>
2016-01-15 10:25:16 -0800

    Update repro steps according to most recent investigation

--- README.md
+++ README.md
@@ -10,10 +10,10 @@ $ node repro.js
 Expected result: the program should exit gracefully.
 Actual result: Most of the time, the program hangs.
 
-However, setting the `CONNECT_TIMEOUT` environment variable to some values
+However, setting the `MAX_CONNECTIONS` environment variable to some values
 will make this program exit as expected most of the time:
 
 ```
-$ CONNECT_TIMEOUT=200 node repro.js
+$ MAX_CONNECTIONS=1 node repro.js
 $
 ```
--- repro.js
+++ repro.js
@@ -6,37 +6,46 @@ function connectToMoray(callback) {
     var MORAY_CLIENT_CONFIG = {
         host: process.env.MORAY_IP || '10.99.99.17',
         port: 2020,
-        log: new bunyan({
-            name: 'moray',
-            level: 'info',
-            serializers: bunyan.stdSerializers
+        log: bunyan.createLogger({
+            name: 'moray_client',
+            level: 'debug',
+            stream: process.stdout
         }),
-        connectTimeout: process.env.CONNECT_TIMEOUT === undefined ?
-        	undefined : +process.env.CONNECT_TIMEOUT
+        maxConnections: process.env.MAX_CONNECTIONS === undefined ?
+            undefined : +process.env.MAX_CONNECTIONS
     };
 
+    console.log('Connecting to moray...');
     var morayClient = moray.createClient(MORAY_CLIENT_CONFIG);
 
     morayClient.once('connect', function onConnect() {
-        console.log('moray client connected!');
+        console.log('Moray client connected!');
         return callback(null, morayClient);
     });
 
     morayClient.on('error', function onError(err) {
-    	console.error('error:', err);
+        console.error('error:', err);
+        return callback(err);
     });
 }
 
 function closeMorayConnection(morayClient, callback) {
-	console.log('closing moray connection...');
-	morayClient.close();
-	return callback();
+    morayClient.on('close', function() {
+      console.log('Moray client connection closed!');
+      return callback();
+    });
+
+    console.log('Closing moray connection...');
+    morayClient.close();
 }
 
 vasync.waterfall([
-	connectToMoray,
-	closeMorayConnection
+    connectToMoray,
+    closeMorayConnection
 ], function allDone(err) {
-	if (err)
-		console.error(err);
+    if (err) {
+        console.error('Error:', err);
+    } else {
+        console.log('All done!');
+    }
 });
